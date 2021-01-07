886502f92df01a5904b1504455c0cad1f6efa830
Duduling <duduling20@gmail.com>
2021-01-07 23:55:49 +0900

    #15.1 React Hook Form

--- src/routers/LoggedInRouter.tsx
+++ src/routers/LoggedInRouter.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-const LoggedInRouter = () => <span>Logged In</span>;
+const LoggedInRouter = () => {
+  return (
+    <div>
+      <h1>Logged In</h1>
+      <button onClick={() => isLoggedInVar()}>Log Out</button>
+    </div>
+  );
+};
 
 export default LoggedInRouter;
