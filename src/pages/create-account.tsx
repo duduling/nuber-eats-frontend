ae6a903b30c83b3df3cf8f576ff5687e6b4db202
Duduling <duduling20@gmail.com>
2021-01-09 23:49:36 +0900

    #15.3 Router and @types

---
+++ src/pages/create-account.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateAccount = () => {
+  return <span>Create Account</span>;
+};
+
+export default CreateAccount;
--- src/routers/LoggedOutRouter.tsx
+++ src/routers/LoggedOutRouter.tsx
@@ -1,57 +1,17 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-interface IForm {
-  email: string;
-  password: string;
-}
+import CreateAccount from "pages/create-account";
+import { Login } from "pages/login";
 
 const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit, errors } = useForm<IForm>();
-
-  const onSubmit = () => {
-    console.log(watch());
-  };
-
-  const onInvalid = () => {
-    console.log("cant create account");
-  };
-
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            ref={register({
-              required: "This is required",
-              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-          />
-        </div>
-        {errors.email?.message && (
-          <span className={"font-bold text-red-600"}>
-            {errors.email?.message}
-          </span>
-        )}
-        {errors.email?.type === "pattern" && (
-          <span className={"font-bold text-red-600"}>Only gmail allowed</span>
-        )}
-        <div>
-          <input
-            ref={register({ required: true })}
-            name="password"
-            type="password"
-            required
-            placeholder="password"
-          />
-        </div>
-        <button className="bg-yellow-300 text-white">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path={"/"} children={<Login />} />
+        <Route path={"/crete-account"} children={<CreateAccount />} />
+      </Switch>
+    </Router>
   );
 };
 
